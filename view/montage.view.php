3b8a26665948455fffcac3a6d7f97800603f365d
Felix PAGE <fpage@e1r13p5.42.fr>
2016-02-19 15:03:11 +0100

    css page principale

--- view/montage.view.php
+++ view/montage.view.php
@@ -1,39 +1,43 @@
 <body onload='init();'>
 
-<div "left-containt">
-<video width='200px' height='100px' name='video'>
-You bower doesn't support video tags </br>
-Pliz uninstall your IE
-</video>
-<canvas width='1000px' height='1000px' style="display:none;"></canvas>
-</br>
-<img width='500px' height='400px' src=""></img>
-</br>
-<input type='button' width='50px' onclick='snapshot()' value='Snapshot'>
-<input type='button' onclick='send("send")' value='Send'>
+<div class="left-containt">
+<div class = "videoAndImage">
+	<video class="video" name='video'>
+	You bower doesn't support video tags </br>
+	Pliz uninstall your IE
+	</video>
+	<canvas width='1000px' height='1000px' style="display:none;"></canvas>
+	</br>
+	<img class="snapshot" src=""></img>
+	</br>
+	<center>
+	<input type='button' width='50px' onclick='snapshot()' value='Snapshot'>
+	<input type='button' onclick='send("send")' value='Send'>
+	</center>
 </div>
+<center>
+	<?php // Get all cliparts
+	echo '<div class="cliparts-containt grid-4">'.PHP_EOL;
 
-<?php // Get all cliparts
-echo '<div class="cliparts-containt grid-4">'.PHP_EOL;
+	$dir = "assets/cliparts/";
+	$list = scandir($dir);
 
-$dir = "assets/cliparts/";
-$list = scandir($dir);
+	foreach ($list as $i => $path)
+	{
+		$path = $dir . $path;
+		if (preg_match("/.*\\.png/", $path))
+			echo '
+			<label>
+			<input class="cliparts-radio" type="radio"
+			name="cliparts" value="'.$path.'">
+			<img class="cliparts" src="' . $path . '">
+			</label>' . PHP_EOL;
+	}
 
-foreach ($list as $i => $path)
-{
-	$path = $dir . $path;
-	if (preg_match("/.*\\.png/", $path))
-		echo '
-		<label>
-		<input class="cliparts-radio" type="radio"
-		name="cliparts" value="'.$path.'">
-		<img class="cliparts" src="' . $path . '">
-		</label>' . PHP_EOL;
-}
-
-echo "</div>".PHP_EOL;
-?>
-
+	echo "</div>".PHP_EOL;
+	?>
+</center>
+</div>
 <?PHP
 	require_once("model/image.model.php");
 	 $imgs = load_images_by_user_id(0);
