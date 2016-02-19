24d83570c512a747880fc9c352b215ad658c6093
Eric DELANGHE <edelangh@e1r12p6.42.fr>
2016-02-19 13:22:03 +0100

    nothing

--- view/montage.view.php
+++ view/montage.view.php
@@ -1,15 +1,19 @@
 <body onload='init();'>
-<input type='button' onclick='snapshot()' value='Snapshot'></button>
-<input type='button' onclick='send("send")' value='Send'></button>
-<img width='500px' height='400px' src=""></img>
-<video width='500px' height='400px' name='video'>
+
+<div "left-containt">
+<video width='200px' height='100px' name='video'>
 You bower doesn't support video tags </br>
 Pliz uninstall your IE
 </video>
 <canvas width='1000px' height='1000px' style="display:none;"></canvas>
+</br>
+<img width='500px' height='400px' src=""></img>
+</br>
+<input type='button' width='50px' onclick='snapshot()' value='Snapshot'>
+<input type='button' onclick='send("send")' value='Send'>
+</div>
 
 <?php // Get all cliparts
-
 echo '<div class="cliparts-containt grid-4">'.PHP_EOL;
 
 $dir = "assets/cliparts/";
@@ -29,6 +33,14 @@ foreach ($list as $i => $path)
 
 echo "</div>".PHP_EOL;
 ?>
+
+<?PHP
+	require_once("model/image.model.php");
+	 $imgs = load_images_by_user_id(0);
+	 foreach ($imgs as $i => $img)
+	 	echo "<img src='" . $img["path"]  . "'></img></br>";
+?>
+
 
 <script>
 'use strict'
